ebbc9f5d35fc15f09100ee99d2f758d553539379
kou.yamada <KOU.YAMADA@grp.tmnf.jp>
2022-09-07 14:59:44 +0900

    aa

---
+++ WebContent/static/js/transition.js
@@ -0,0 +1,29 @@
+ function toNewEstimationStartPage(){
+
+	location.href = "./toNewEstimationStartPage"
+
+}
+
+ function toRecordStartPage(){
+
+	location.href = "./toRecordStartPage"
+
+}
+
+ function toQueryStartPage(){
+
+	location.href = "./toQueryStartPage"
+
+}
+
+ function toTerminationStartPage(){
+
+	location.href = "./toTerminationStartPage"
+
+}
+
+ function toAccidentStartPage(){
+
+	location.href = "./toAccidentStartPage"
+
+}
