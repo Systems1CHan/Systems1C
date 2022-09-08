13b8a234f6f5954a40fcbb63db35718d25d9b56d
tooru.nakaya <tooru.nakaya@grp.tmnf.jp>
2022-09-08 13:57:32 +0900

    証券番号データチェッククラス作成完了

--- src/TS_Net/model/datacheck/PolNoChecker.java
+++ src/TS_Net/model/datacheck/PolNoChecker.java
@@ -25,7 +25,7 @@ import TS_Net.model.data.ContractInfo;
 public class PolNoChecker {
 
 	/**
-	 * チェックメソッド。
+	 * 証券番号・事故受付番号入力チェック(0001)メソッド。
 	 * <p>
 	 *契約情報オブジェクトの証券番号、
 	 *事故受付情報オブジェクトの事故受付番号
@@ -35,7 +35,7 @@ public class PolNoChecker {
 	public String polNoInputCheck(ContractInfo contractInfo) {
 
 
-		if(((contractInfo.getPolNo() == null) || ("".equals(contractInfo.getPolNo()))) && ((contractInfo.getClaimNo() == null) || ("".equals(contractInfo.getClaimNo())))){
+		if(((contractInfo.getPolNo() != null) || (contractInfo.getPolNo().length() != 0)) && ((contractInfo.getClaimNo() != null) || (contractInfo.getClaimNo().length() != 0))){
 
 			return ErrorMsgConst.FORM_ERROR0001;
 
@@ -43,5 +43,72 @@ public class PolNoChecker {
 		return null;
 		}
 	}
+
+
+	/**
+	 * 証券番号・事故受付番号入力チェック(0018)メソッド。
+	 * <p>
+	 *契約情報オブジェクトの証券番号、
+	 *事故受付情報オブジェクトの事故受付番号がともに未入力の場合、
+	 *エラーメッセージを返却する。
+	 * </p>
+	 */
+	public String polNoNotInputCheck(ContractInfo contractInfo) {
+
+
+		if(((contractInfo.getPolNo() == null) || ("".equals(contractInfo.getPolNo()))) && ((contractInfo.getClaimNo() == null) || ("".equals(contractInfo.getClaimNo())))){
+
+			return ErrorMsgConst.FORM_ERROR0018;
+
+		}else {
+		return null;
+		}
+	}
+
+	/**
+	 * 証券番号存在チェック(0006)メソッド。
+	 * <p>
+	 *このメソッドが呼び出された場合、以下の処理を行う。
+	 *1. 契約情報テーブルの証券番号のカラムに該当証券番号があるかを調べる
+	 *2. 該当証券番号があった場合、"1"が、ない場合"0"を返す
+	 *3. ある場合はnullを戻り値として返す
+	 *4. ない場合は、エラーメッセージを戻り値として返す
+	 * </p>
+	 */
+	public String polNoExistenceCheck(ContractInfo contractInfo) {
+
+
+		if((contractInfo.getPolNo() == null) || ("".equals(contractInfo.getPolNo()))){
+
+			return ErrorMsgConst.FORM_ERROR0006;
+
+		}else {
+		return null;
+		}
+	}
+
+	/**
+	 * 証券番号解約済みチェック(0007)メソッド。
+	 * <p>
+	 *このメソッドが呼び出された場合、以下の処理を行う。
+	 *1. 契約情報テーブルの該当印刷連番と一致する行を探す
+	 *2. 一致した行の解約フラグの値を取り出す
+	 *3. 取り出した値が"0"（通常）の場合、nullを返す
+	 *4. 取り出した値が"1"（解約）の場合、エラーメッセージを戻り値として返す
+	 * </p>
+	 */
+	public String polNoExistenceCheck(ContractInfo contractInfo) {
+
+
+		if(contractInfo.getCancelFlg() == "1"){
+
+			return ErrorMsgConst.FORM_ERROR0007;
+
+		}else if(contractInfo.getCancelFlg() == "0") {
+		return null;
+		}
+	}
+
+
 
 }
