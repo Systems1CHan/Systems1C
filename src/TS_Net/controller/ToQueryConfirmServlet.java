f1c1e662dad9f80efcc22999cd8d6647a015a0f9
tooru.nakaya <tooru.nakaya@grp.tmnf.jp>
2022-09-08 16:31:43 +0900

    サーブレット未完成。補償データクラスを一部修正（getCompensationの引数をIntegerからStringへ）

--- src/TS_Net/controller/ToQueryConfirmServlet.java
+++ src/TS_Net/controller/ToQueryConfirmServlet.java
@@ -12,7 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import TS_Net.model.constant.ErrorMsgConst;
 import TS_Net.model.constant.SystemConst;
+import TS_Net.model.dao.CompensationDao;
 import TS_Net.model.dao.ContractInfoDao;
+import TS_Net.model.data.Compensation;
 import TS_Net.model.data.ContractInfo;
 import TS_Net.model.datacheck.BlankChecker;
 
@@ -25,20 +27,24 @@ public class ToQueryConfirmServlet extends HttpServlet {
 		//文字化けを防止する。
 		request.setCharacterEncoding(SystemConst.CHAR_SET);
 		String page = "/WEB-INF/view/QueryConfirm.jsp";
-
+		//dataオブジェクトを生成する。
 		ContractInfo contractInfo = null;
+		Compensation compensation = null;
 		//DAOを生成する。
-		ContractInfoDao dao = new ContractInfoDao();
+		ContractInfoDao contractInfoDao = new ContractInfoDao();
+		CompensationDao compensationDao = new CompensationDao();
 
 		//contractFormCheckerオブジェクトを生成。
 		BlankChecker blankChecker = new BlankChecker();
 
+		String polNo = request.getParameter("polNo");
+
 		//チェック結果がnull出ない場合はリクエストスコープのエラーメッセージをセットし、契約内容入力画面を返す。
-		if (blankChecker.blankCheck(request.getParameter("polNo")) != null) {
+		if (blankChecker.blankCheck(polNo) != null) {
 
-			request.setAttribute("FORM_ERROR", contractFormChecker.check(rsv));
+			request.setAttribute("FORM_ERROR", blankChecker.blankCheck(polNo));
 
-			page ="/WEB-INF/view/ContractFormPage.jsp";
+			page ="/WEB-INF/view/QueryStart.jsp";
 			//契約内容入力画面へforwardする。
 			RequestDispatcher rd = request.getRequestDispatcher(page);
 			rd.forward(request, response);
@@ -47,8 +53,11 @@ public class ToQueryConfirmServlet extends HttpServlet {
 		}
 
 		try {
-			dao.connect();
-			rsv.setAmount(dao.getPrice(request.getParameter("comeFrom"), request.getParameter("comeTo")));
+			contractInfoDao.connect();
+			//証券番号に合致する契約情報を取得し、オブジェクトに格納する。
+			contractInfo = contractInfoDao.getContractInfo(polNo);
+
+
 		} catch (ClassNotFoundException | SQLException e) {
 
 			e.printStackTrace();
@@ -63,7 +72,7 @@ public class ToQueryConfirmServlet extends HttpServlet {
 
 		finally {
 			try {
-				dao.close();
+				contractInfoDao.close();
 
 			} catch(SQLException e) {
 				request.setAttribute("ERROR", ErrorMsgConst.SYSTEM_ERROR);
@@ -72,6 +81,40 @@ public class ToQueryConfirmServlet extends HttpServlet {
 			}
 
 		}
+
+
+
+
+		try {
+			compensationDao.connect();
+			//証券番号に合致する契約情報を取得し、オブジェクトに格納する。
+			compensation = contractInfoDao.getCompensation(contractInfo.getInsatsuRenban());
+
+
+		} catch (ClassNotFoundException | SQLException e) {
+
+			e.printStackTrace();
+			request.setAttribute("ERROR", ErrorMsgConst.SESSION_ERROR);
+			// システムエラー画面を戻り値に設定する。
+			page = "/WEB-INF/view/ErrorPage.jsp";
+			//システムエラー画面へforwardする。
+			RequestDispatcher rd = request.getRequestDispatcher(page);
+			rd.forward(request, response);
+
+		}
+
+		finally {
+			try {
+				contractInfoDao.close();
+
+			} catch(SQLException e) {
+				request.setAttribute("ERROR", ErrorMsgConst.SYSTEM_ERROR);
+				// システムエラー画面を戻り値に設定する。
+				page = "/WEB-INF/view/ErrorPage.jsp";
+			}
+
+		}
+
 
 		/* TOPメニューJSPへforwardする。*/
 		RequestDispatcher rd = request.getRequestDispatcher(page);
