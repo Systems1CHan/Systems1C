44e332cce91440adf3be44bdb20d713a4109f92b
tooru.nakaya <tooru.nakaya@grp.tmnf.jp>
2022-09-21 16:30:13 +0900

    AWSからローカルへ変数書き換え

--- src/TS_Net/model/constant/SystemConst.java
+++ src/TS_Net/model/constant/SystemConst.java
@@ -18,39 +18,39 @@ package TS_Net.model.constant;
  */
 public class SystemConst {
 
-//
-//	/** ドライバー名 */
-//	public static final String JDBC_DRIVER_NAME = "com.mysql.jdbc.Driver";
-//
-//	/** JDBC接続文字列 */
-//	public static final String JDBC_URL = "jdbc:mysql://localhost:3306/final_exercise_db?allowPublicKeyRetrieval=true&useSSL=false";
-//
-//	/** ユーザー名 */
-//	public static final String USER = "user_name";
-//
-//	/** パスワード */
-//	public static final String PASSWORD = "mysql";
-//
-//	/** 文字コード */
-//	public static final String CHAR_SET = "UTF-8";
-//
-//
-//}
 
-	//aws用
 	/** ドライバー名 */
-	public static final String JDBC_DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+	public static final String JDBC_DRIVER_NAME = "com.mysql.jdbc.Driver";
 
 	/** JDBC接続文字列 */
-	public static final String JDBC_URL = "jdbc:mysql://kouyamada085-dbserver1.cdipzmdeylgh.ap-northeast-1.rds.amazonaws.com:3306/final_exercise_db?allowPublicKeyRetrieval=true&useSSL=false";
+	public static final String JDBC_URL = "jdbc:mysql://localhost:3306/final_exercise_db?allowPublicKeyRetrieval=true&useSSL=false";
 
 	/** ユーザー名 */
-	public static final String USER = "admin";
+	public static final String USER = "user_name";
 
 	/** パスワード */
-	public static final String PASSWORD = "ha1cndyni";
+	public static final String PASSWORD = "mysql";
 
 	/** 文字コード */
 	public static final String CHAR_SET = "UTF-8";
 
+
 }
+
+//	//aws用
+//	/** ドライバー名 */
+//	public static final String JDBC_DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+//
+//	/** JDBC接続文字列 */
+//	public static final String JDBC_URL = "jdbc:mysql://kouyamada085-dbserver1.cdipzmdeylgh.ap-northeast-1.rds.amazonaws.com:3306/final_exercise_db?allowPublicKeyRetrieval=true&useSSL=false";
+//
+//	/** ユーザー名 */
+//	public static final String USER = "admin";
+//
+//	/** パスワード */
+//	public static final String PASSWORD = "ha1cndyni";
+//
+//	/** 文字コード */
+//	public static final String CHAR_SET = "UTF-8";
+//
+//}
